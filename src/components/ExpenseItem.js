c4e0baf26d9ac0d40b500fa6e609913117d48bdc
elee489 <46901804+elee489@users.noreply.github.com>
2021-06-15 12:56:37 -0400

    Dynamic Input

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+
+    const expenseDate = new Date(2021, 3, 15);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 300.00;
   return (
     <div className = "expense-item">
-      <div>June 15th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className = "expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className = "expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className = "expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
